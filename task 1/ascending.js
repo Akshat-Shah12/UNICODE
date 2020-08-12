dc8f414b449fbbb81900fc5c20bd7fc0c49b62a3
Akshat Shah <60980876+Akshat-Shah12@users.noreply.github.com>
2020-08-12 11:32:44 +0530

    Update ascending.js

--- task 1/ascending.js
+++ task 1/ascending.js
@@ -2,32 +2,27 @@
 var file =[{
     "name":process.argv[2],
     "surname":process.argv[3],
-    "mathMarks":process.argv[4],
-    "englishMarks":process.argv[5]},
+    "mathMarks":parseFloat(process.argv[4]),
+    "englishMarks":parseFloat(process.argv[5])},
+
+
 {
     "name":process.argv[6],
     "surname": process.argv[7],
-    "mathMarks":process.argv[8],
-    "englishMarks":process.argv[9]
+    "mathMarks":parseFloat(process.argv[8]),
+    "englishMarks":parseFloat(process.argv[9])
 },
 {
     "name":process.argv[10],
     "surname": process.argv[11],
-    "mathMarks":process.argv[12],
-    "englishMarks":process.argv[13]
-}] 
+    "mathMarks":parseFloat(process.argv[12]),
+    "englishMarks":parseFloat(process.argv[13])
+}]
 
 function rank(file){
-    file = JSON.parse(file)//converting back to object
-    file.sort((a,b)=>{   //sorting it (returning -1 swaps the place)
-        if((a.mathMarks+a.englishMarks)/2>(b.mathMarks+b.englishMarks)/2){
-            return -1;
-        }
-        if((a.mathMarks+a.englishMarks)/2<(b.mathMarks+b.englishMarks)/2){
-            return 1;
-        }
-        return 0;
-    })
+  file.sort((a, b) => parseFloat((b.mathMarks+b.englishMarks)/2) - parseFloat((a.mathMarks+a.englishMarks)/2));
+
+
     console.log(file)
 }
-rank(JSON.stringify(file));//converting to string
+rank(file);//converting to string
