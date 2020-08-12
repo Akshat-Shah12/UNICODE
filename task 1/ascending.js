86797c6072aa4c536a64c70d0d7a225f6247a4ca
Akshat Shah <60980876+Akshat-Shah12@users.noreply.github.com>
2020-08-12 09:29:07 +0530

    Add files via upload

---
+++ task 1/ascending.js
@@ -0,0 +1,33 @@
+
+var file =[{
+    "name":process.argv[2],
+    "surname":process.argv[3],
+    "mathMarks":process.argv[4],
+    "englishMarks":process.argv[5]},
+{
+    "name":process.argv[6],
+    "surname": process.argv[7],
+    "mathMarks":process.argv[8],
+    "englishMarks":process.argv[9]
+},
+{
+    "name":process.argv[10],
+    "surname": process.argv[11],
+    "mathMarks":process.argv[12],
+    "englishMarks":process.argv[13]
+}] 
+
+function rank(file){
+    file = JSON.parse(file)//converting back to object
+    file.sort((a,b)=>{   //sorting it (returning -1 swaps the place)
+        if((a.mathMarks+a.englishMarks)/2>(b.mathMarks+b.englishMarks)/2){
+            return -1;
+        }
+        if((a.mathMarks+a.englishMarks)/2<(b.mathMarks+b.englishMarks)/2){
+            return 1;
+        }
+        return 0;
+    })
+    console.log(file)
+}
+rank(JSON.stringify(file));//converting to string
